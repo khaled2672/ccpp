ff5829afe0f908efcd528fa9a1be83e268c418b7
khaled2672 <mohamedkh2672@gmail.com>
2025-04-16 05:45:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import joblib
+import os
 
 # 🔹 Set page config
 st.set_page_config(page_title="Gas Turbine Power Prediction", page_icon="⚡")
