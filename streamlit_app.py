e67185fc8795e32c7c4c0a2d5eedfcb03e9e5493
khaled2672 <mohamedkh2672@gmail.com>
2025-04-18 21:46:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 import joblib
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.metrics import mean_absolute_error
 
 # 1. SET PAGE CONFIG (MUST BE FIRST STREAMLIT COMMAND)
