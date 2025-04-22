cd806f81e41c90c3570f377171acee0a5c24ce3b
khaled2672 <mohamedkh2672@gmail.com>
2025-04-23 01:41:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -101,10 +101,7 @@ if uploaded_file is not None:
     if df_processed is not None:
         st.write("✅ Dataset Columns Mapped Successfully")
 
-        features = df_processed[["Ambient Temperature", "Ambient Relative Humidity", "Ambient Pressure", "Exhaust Vacuum"]]
-        rf_preds = models['rf_model'].predict
-        xgb_preds = models['xgb_model'].predict
-
+        
         if auto_optimize and 'Actual Power' in df_processed.columns:
             y_true = df_processed['Actual Power'].values
             weight, best_mae = optimize_weight(rf_preds, xgb_preds, y_true)
