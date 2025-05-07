2c5a76b73d6d4ceb0cc72a6ef44ba8f4ca8c18d0
khaled2672 <mohamedkh2672@gmail.com>
2025-05-07 18:04:55 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import joblib
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 from io import StringIO
 
 # Theme configuration
