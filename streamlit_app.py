9230125d80247fe7aa115ece0f99a713e7dd22cc
khaled2672 <mohamedkh2672@gmail.com>
2025-05-08 09:35:41 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ def set_theme(dark):
             """
             <style>
             .stApp {
-                background-image: url("https://images.unsplash.com/photo-1508514177221-188b1cf16e9d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1920&q=80");
+                background-image: url("https://www.enka.com/wp-content/uploads/freshizer/015ac6b46b92087855b6591cfe219b8c_IZMIR_010-800-400-c.jpg");
                 background-size: cover;
                 background-attachment: fixed;
                 background-position: center;
