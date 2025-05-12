44db1c28fd78e5d049b173d3b1f223aa210f351e
khaled2672 <mohamedkh2672@gmail.com>
2025-05-12 12:06:50 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from io import StringIO
 
 # Theme configuration with background images
 
-def set\_theme(dark):
+def set_theme(dark):
 plt.style.use('dark\_background' if dark else 'default')
 if dark:
 st.markdown(
